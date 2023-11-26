7048a576219aa855f97a1864b1828e54339cfc7c
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2023-11-26 20:48:35 +0600

    JavaScript#2

--- JavaScript.js
+++ JavaScript.js
@@ -32,12 +32,155 @@
 // let user;
 // console.log(user ?? "Аноним")
 
-let a = 10;
-while (a>0){
-    console.log(a)
-    a--;
+// let a = 10;
+// while (a>0){
+//     console.log(a)
+//     a--;
+// }
+
+// for(let i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+// let c = 2 + 2;
+// switch (c){
+//     case 3:
+//         console.log("Мало");
+//         break;
+//     case 4:
+//         console.log("Норм");
+//         break;
+//     case 5:
+//         console.log("Много");
+//         break;
+//     default:
+//         console.log("Нет такого")
+// }
+
+// function showMessage(){
+//     console.log("dfgjdgj")
+// }
+// showMessage();
+// showMessage();
+// showMessage();
+
+// let xyz = prompt("Как тебя зовут")
+// console.log(xyz)
+
+// let x = prompt("Первое число")
+// let y = prompt("Второе число")
+
+// console.log(x.charAt(0))
+// console.log(y.charAt(0))
+
+
+// if (x.charAt(0) == y.charAt(0)) {
+//     console.log('Совпадают');
+// }else{
+//     console.log("Не совпадают");
+// }
+
+
+// let a = prompt("Введите строку")
+// s = a.length
+
+// for (let i = s; i>0; i--){
+//     console.log(a.charAt(i))
+// }
+
+
+// function coll(){
+//     let a = prompt("Число для Гипотезы Коллатца")
+//     while (a != 1){
+//         if (a % 2 == 0){
+//             a = a / 2
+//         }else{
+//             a = ((a * 3) + 1)
+//         }
+//     console.log(a)
+// }
+// }
+
+
+// let name = prompt("Напишите имя")
+// let age = prompt("Уточните возраст")
+
+// function name_age(){
+
+//     l = name + " имеет возраст " + age + " и он зрелый "
+//     console.log(l)
+//     coll()
+// }
+// name_age()
+
+
+
+function massif(){
+    let flag = true;
+    let n = prompt("Введите длину массива: ");
+    let num = [];
+    for(let i = 0; i < n; i++){
+        let m = prompt("Число: ")
+        num.push(m)
+    }
+    for(let j = 0; j < num.length; j++){
+        if (num[j] % 2 != 0) {
+            flag = false;
+            break;
+        }
+    }
+
+    if (flag == false){
+        console.log('Нечетное');
+    } else {
+        console.log('Все четные');
+    }
 }
 
-for(let i = 0; i < 10; i++){
-    console.log(i)
+massif()
+
+function massif(){
+    let flag = false;
+    let n = prompt("Введите длину массива: ");
+    let num = [];
+    for(let i = 0; i < n; i++){
+        let m = prompt("Число: ")
+        num.push(m)
+    }
+    for(let j = 0; j < num.length; j++){
+        if (num[j] % 2 != 0) {
+            flag = true;
+            break;
+        }
+    }
+
+    if (flag == true){
+        console.log('Один элемент нечетный');
+    } else {
+        console.log('Все элементы четные');
+    }
 }
+
+massif()
+
+function massif(){
+    let flag = false;
+    let n = prompt("Введите длину массива: ");
+    let num = [];
+    let num5 = [];
+    for(let i = 0; i < n; i++){
+        let m = prompt("Число: ")
+        num.push(m)
+    }
+    for(let j = 0; j < num.length; j++){
+        if (num[j] % 5 == 0) {
+            num5.push(num[j])
+        }
+    }
+    console.log(num5)
+
+    
+}
+
+massif()
+
