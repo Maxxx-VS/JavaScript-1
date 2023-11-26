48b4dc60e8058de0835db6607639542ac472b765
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2023-11-26 18:44:43 +0600

    JavaScript#1

---
+++ JavaScript.js
@@ -0,0 +1,43 @@
+// // alert('Привет Мир')
+// // alert(5/2)
+
+// var a;
+// let x;
+// x = "Hello"
+// const y = 10;
+// a = null;
+
+// console.log(x, y, a)
+// console.log(typeof x);
+// console.log(5/2, 5%2);
+// console.log(4 ** (1/2));
+// let s = "my" + "string"
+// console.log(s);
+
+// let year = 2015;
+// if (year == 2015) console.log('2015');
+// if (year == 2012) {
+//     console.log("Good")
+//     console.log("Buy")
+// }else if (year > 2015){
+//     console.log("> 2015");
+// }else{
+//     console.log("Vau");
+// }
+
+// let age = 19;
+// let TreanrOperator = (age > 18) ? true : false;
+// console.log(TreanrOperator)
+
+// let user;
+// console.log(user ?? "Аноним")
+
+let a = 10;
+while (a>0){
+    console.log(a)
+    a--;
+}
+
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
