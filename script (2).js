6e025f820460e035298a67df8f83d47286a68305
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2023-12-12 21:45:04 +0600

    Калькулятор

---
+++ script (2).js
@@ -0,0 +1,97 @@
+var size = 3;
+var colors = [];
+var pickedColor;
+var squares = document.querySelectorAll(".square");
+var colorDisp = document.querySelector("h1");
+var mes = document.querySelector("#message");
+var reset = document.querySelector("#reset");
+var mode = document.querySelectorAll(".mode");
+
+init();
+
+reset.addEventListener("click", function(){
+  resetColors();
+});
+
+function init(){
+  setMode();
+  setSquares();
+  resetColors();
+}
+
+function changeColor(color){
+  for(var i=0; i < squares.length; i++){
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor(){
+  var a = Math.floor(Math.random()*colors.length);
+  return colors[a];
+}
+
+function generateRandomColors(size){
+  var arr = [];
+  for(var i=0; i<size; i++){
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor(){
+  var r = Math.floor(Math.random()*256);
+  var g = Math.floor(Math.random()*256);
+  var b = Math.floor(Math.random()*256);
+  return "rgb(" + r +", " + g + ", " +b + ")";
+}
+
+function resetColors(){
+  reset.textContent = "Новый цвет";
+  mes.textContent = "";
+  colors = generateRandomColors(size);
+  pickedColor = pickColor();
+  colorDisp.textContent = pickedColor;
+  document.querySelector("header").style.backgroundColor = document.body.style.backgroundColor;
+
+  for(var i=0; i<squares.length; i++){
+    if (colors[i]){
+      squares[i].style.backgroundColor = colors[i];
+      squares[i].style.display = "block";
+    } else {
+      squares[i].style.display = "none";
+    }
+  }
+}
+
+function setMode(){
+  for(var i=0; i<mode.length; i++){
+    mode[i].addEventListener("click", function(){
+      mode[0].classList.remove("selected");
+      mode[1].classList.remove("selected");
+      mode[2].classList.remove("selected");     
+      this.classList.add("selected");
+      if (this.textContent === "*Легкий*"){size = 3}
+      else if (this.textContent === "*Сложный*"){size = 6}
+      else {size = 9}
+      //this.textContent === "Легкий" ? size = 6 : size = 9;
+      resetColors();
+    });
+  }
+}
+
+function setSquares(){
+  for(var i=0; i < squares.length; i++){
+    squares[i].addEventListener("click", function(){
+      var clickedColor = this.style.backgroundColor;
+      if (clickedColor === pickedColor){
+        mes.textContent = "Правильно!";
+        reset.textContent = "Играть еще раз?";
+        changeColor(pickedColor);
+        document.querySelector("header").style.backgroundColor = pickedColor;
+      } else {
+        this.style.backgroundColor = document.body.style.backgroundColor;
+        mes.textContent = "Это был " + clickedColor + ". Попробуй еще раз!";
+      }
+    });
+  }
+}
