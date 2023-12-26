42756500b456980dc616602e4aa96804d9f2f61a
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2023-12-26 20:26:42 +0600

    Анимация-2

--- Анимация/Anim.js
+++ Анимация/Anim.js
@@ -2,18 +2,27 @@ var canvas = document.querySelector('.snow'),
     ctx = canvas.getContext('2d'),
     windowW = window.innerWidth,
     windowH = window.innerHeight,
-    numFlakes = 200,
-    flakes = [];
+    numFlakes = 200, //кол-во снежков
+    flakes = [];  //массив для снежков
 
 function Flake(x, y) {
     var maxWeidht = 5,
         maxSpeed = 3;
-} 
 
-this.x = x;
-this.y = y;
-this.r = randomBetween(0, 1);
-this.a = randomBetween(0, Math.PI);
-this.aStep = 0.01;
 
-this.weight = randomBetween(2, maxWeidht);
+this.x = x; //координаты снежков
+this.y = y; //координаты снежков
+this.r = randomBetween(0, 1);  //радиус снежков
+this.a = randomBetween(0, Math.PI);  //угол под которым снежинка движется
+this.aStep = 0.01; //шаг изменения угла или растояния
+
+this.weight = randomBetween(2, maxWeidht); //объем
+this.alpha = (this.weight / maxWeidht); //разница
+this.speed = (this.weight / maxWeidht) * maxSpeed; //скорость
+
+this.update = function() {
+    this.x += Math.cos(this.a) * this.r;
+    this.a += this.aStep;
+    this.y += this.speed;
+    }
+}
