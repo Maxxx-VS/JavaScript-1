05d8f1e81249daf0d11a8ed74b27bbfdda848cb3
Высочанский Максим <126272688+Maxxx-VS@users.noreply.github.com>
2023-12-26 21:16:49 +0600

    Анимация 3

--- Анимация/Anim.js
+++ Анимация/Anim.js
@@ -9,7 +9,6 @@ function Flake(x, y) {
     var maxWeidht = 5,
         maxSpeed = 3;
 
-
 this.x = x; //координаты снежков
 this.y = y; //координаты снежков
 this.r = randomBetween(0, 1);  //радиус снежков
@@ -22,8 +21,8 @@ this.speed = (this.weight / maxWeidht) * maxSpeed; //скорость снежк
 
 this.update = function() { //обновляет состояние снежков
     this.x += Math.cos(this.a) * this.r; //позволяет снежк перемещаться по горизонтали в зависимости от угла и радиуса
-    this.a += this.aStep; //
-    this.y += this.speed;
+    this.a += this.aStep; //эффек поворота снежка
+    this.y += this.speed; //скорость снежка
     }
 }
 
@@ -35,9 +34,55 @@ function init() {
     
     while (i--) {
         x = randomBetween(0, windowW, true);
-        y = randomBetween(0, windowH, true)
+        y = randomBetween(0, windowH, true);
+
+        flake = new Flake(x, y);
+        flakes.push(fake);
     }
-
-
-
+    scaleCanvas(); //Изменяет размер холста
+    loop(); //анимирукт как в ТКинтер
 }
+
+function scaleCanvas() {
+    canvas.widht = windowW;
+    canvas.height = windowH;
+}
+
+function loop() {
+    var i = flakes.length,
+        z,
+        dist,
+        flakeA,
+        flakeB;
+}
+
+ctx.save(); //очистить холст
+ctx.setTransform(1, 0, 0, 1, 0, 0);
+ctx.clearRect(0, 0, windowW, windowH); //прозрачный прямоугольник удаляющий предыдущий кадр
+ctx.restore();
+
+while (i--) { //для каждой снежинки
+    flakeA = flakes[i];
+    flakeA.update();
+    ctx.beginPath();
+    ctx.arc(flakeA.x, flakeA.y, flakeA.weight, 0, 2 + Math.PI, false);
+    ctx.fillStyle = 'rgba(225, 225, 225, ' + flakeA.alpha + ')';
+    ctx.fill();
+
+    if (flakeA.y >= windowH) {
+        flakeA.y + -flakeA.weight;
+    }
+}
+
+requestAnimationFrame(loop);
+
+
+function randomBetween(min, max, round) {
+    var num = Math.random() + (max - min + 1) + min;
+    if (round) {
+        return Math.floor(num);
+    } else {
+        return num;
+    }
+}
+
